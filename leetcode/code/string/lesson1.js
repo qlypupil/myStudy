ed5eaeeba8f86802f3d9a830d7341bc9359450f1
秦历阳 <liyang.qing@yungu.org>
2019-07-18 09:51:34 +0800

    反转字符串中的单词 557

---
+++ leetcode/code/string/lesson1.js
@@ -0,0 +1,60 @@
+// leetcode 557
+/**
+ * 知识点
+ * String.prototype.split 将字符串分割为字符串数组
+ * Array.prototype.map  创建一个新数组，结果是该数组中的每一个元素都调用一个提供的函数后返回的结果
+ * Array.prototype.reverse  颠倒数组中元素的顺序
+ * Array.prototype.join  将所有元素放入一个字符串，元素通过指定的分隔符进行分割
+ */
+
+export default str => {
+  // 字符串按空格进行分割，保存数组，数组的元素的先后顺序就是单词的顺序
+  // 对数组进行遍历，然后每个元素进行反转
+  return str
+    .split(' ')
+    .map(item => {
+      return item
+        .split('')
+        .reverse()
+        .join('');
+    })
+    .join(' ');
+};
+
+// export default str => {
+//   let arr = s.split(' ');
+//   let result = arr.map(item => {
+//     return item
+//       .split('')
+//       .reverse()
+//       .join('');
+//   });
+//   return result.join(' ');
+// };
+
+/* 比上面的方法更优
+let reverseWords = function(s) {
+  let finalword = '';
+  let spaceword = s.split(' ');
+  for (let i in spaceword) {
+    spaceword[i] = spaceword[i]
+      .split('')
+      .reverse()
+      .join('');
+    finalword = finalword + spaceword[i] + ' ';
+  }
+  return finalword.replace(/(\s*$)/g, '');
+};
+*/
+
+/* leetcode 最优解
+const reverseWords = str => {
+  return str
+    .split('')
+    .reverse()
+    .join('')
+    .split(' ')
+    .reverse()
+    .join(' ')
+}
+*/
